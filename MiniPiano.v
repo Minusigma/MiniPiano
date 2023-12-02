7bd24e71a2846ed4b34e6559156b81e07f36ac52
Minusigma <132658637+Minusigma@users.noreply.github.com>
2023-12-02 15:19:07 +0800

    Create MiniPiano.v

---
+++ MiniPiano.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/12/02 15:11:33
+// Design Name: 
+// Module Name: MiniPiano
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MiniPiano(
+    );
+endmodule
